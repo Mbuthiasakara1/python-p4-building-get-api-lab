8f9931534026a8e431eeb5dd76d3bd60152fb793
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-10-06 12:19:15 +0300

    get api lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,40 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
-
+    bakery=[]
+    for b in Bakery.query.all():
+        bakery_dict=b.to_dict()
+        bakery.append(bakery_dict)
+    response=make_response(jsonify(bakery),200)
+    return response
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery=Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response=make_response(jsonify(bakery_dict),200)
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    # Convert the baked goods to dictionaries
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+    # Return the JSON response
+    response = make_response(jsonify(baked_goods_list), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    # Query the baked goods ordered by price in descending order and limit to 1
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    
+    # Convert the most expensive baked good to a dictionary
+    most_expensive_dict = most_expensive.to_dict() if most_expensive else {}
+    
+    # Return the JSON response
+    response = make_response(jsonify(most_expensive_dict), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
